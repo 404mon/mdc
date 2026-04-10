cae82fc3898e833e76a3c3b507a82d2655f8578f
404mon <daemn404@gmail.com>
2026-04-10 23:44:50 +0200

    Initial commit: Struttura base e configurazione CMake

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("--- MDC: Minimalist Desktop C-Notes ---\n");
+    printf("Versione: 0.0.1 (Pre-Alpha)\n");
+    printf("Stato: Inizializzazione...\n");
+    
+    return 0;
+}
